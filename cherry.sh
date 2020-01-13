6c82c764074a1b28aa697879b68b8fd5c4efb3b1
Vitor Subhi <VitorAlmeida.Subhi@philips.com>
2020-01-13 09:05:34 -0300

    Rename Variables

--- cherry.sh
+++ cherry.sh
@@ -5,23 +5,23 @@ echo 'Comentário:'
 read -p "-> " comments
 echo 'Digite o(s) número(s) da(s) Versão(ões):    -- OBS: Em casos de múltiplas versões, separe com espaços.'
 echo 'Exemplo: 3.05.1765 3.05.1764 3.05.1764'
-	read -p "-> " -a number_version
+	read -p "-> " -a array_version
 echo 'Digite o(s) Hashe(s):   -- OBS: Em casos de múltiplos hashes, separe com espaços.'
-	read -p "-> " -a hash
-for i in ${number_version[@]}
+	read -p "-> " -a array_hash
+for version in ${array_version[@]}
 do
-	echo "Trocando para branch ${i}"
-	git checkout ${i} 
-	echo "Atualizando Branch ${i}"
-	git pull origin ${i}
-	echo "Criando branch ${i}_${number_os}"
-	git checkout -b ${i}_${number_os} 
-	for j in ${hash[@]}
+	echo "Trocando para branch ${version}"
+	git checkout ${version} 
+	echo "Atualizando Branch ${version}"
+	git pull origin ${version}
+	echo "Criando branch ${version}_${number_os}"
+	git checkout -b ${version}_${number_os} 
+	for hash in ${array_hash[@]}
 	do
-		echo "Adicionado Cherry-Pick do hash ${j}"
+		echo "Adicionado Cherry-Pick do hash ${hash}"
 		git cherry-pick ${hash}
 	done
 	echo "Realizando Push"
-	git push origin ${i}_${number_os}
+	git push origin ${version}_${number_os}
 done
 echo "Script finalizado!"
