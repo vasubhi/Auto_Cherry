218be247cff66d7af3ac663f1a87c1bfbbcf4dc1
Vitor Subhi <VitorAlmeida.Subhi@philips.com>
2020-01-24 08:41:24 -0300

    Removed comments (Unused)

--- cherry.sh
+++ cherry.sh
@@ -1,8 +1,6 @@
 echo 'Antes de Continuar, certifique-se que não possui nenhum arquivo no git status que não queira submeter.'
 echo 'Digite o número da OS:'
 read -p "-> " number_os
-echo 'Comentário:'
-read -p "-> " comments
 echo 'Digite o(s) número(s) da(s) Versão(ões):    -- OBS: Em casos de múltiplas versões, separe com espaços.'
 echo 'Exemplo: 3.05.1765 3.05.1764 3.05.1764'
 while [[ -z "$array_version" ]]
